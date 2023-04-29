abedbeb706d9259abc393e64d9549c72171bb8af
Abhishek Kumar Yadav <abhisheksinghyadav0505@gmail.com>
2023-04-29 23:19:03 +0530

    added meaningful data in menu api

--- src/components/MenuApi.js
+++ src/components/MenuApi.js
@@ -6,44 +6,44 @@ const Menu = [
       category: "breakfast",
       price: "12₹",
       description:
-        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Looking for a quick and delicious meal that will satisfy your hunger? Look no further than our Maggi noodles! Our Maggi is a classic comfort food that has been enjoyed by generations around the world.",
     },
   
     {
       id: 2,
       image: "images/allupakoida.jpg",
-      name: "allu pakoda",
+      name: "Aalu Pakoda",
       category: "evening",
       price: "20₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        " aalu pakoda is perfect for sharing with friends and family, or as a quick snack for your busy schedule. It's a classic street food that has been enjoyed for generations, and we take pride in making it fresh and hot for your ultimate enjoyment.",
     },
     {
       id: 3,
       image: "images/corn.jpg",
-      name: "corn",
+      name: "Corn",
       category: "breakfast",
       price: "10₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Come and taste our delicious corn dish today and savor the sweet and savory flavors that will transport you to a world of culinary delight!",
     },
     {
       id: 4,
       image: "../images/chola.jpg",
-      name: "chola",
+      name: "Chhola",
       category: "lunch",
       price: "50₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Each bite of our chhola is a delightful explosion of flavors, with a perfect balance of sweet, tangy, and spicy notes that will tantalize your taste buds. Whether you're a fan of Indian cuisine or looking to try something new and exciting, our chhola dish is the perfect choice.",
     },
     {
       id: 5,
       image: "../images/pizza.jpg",
-      name: "pizza",
+      name: "Pizza",
       category: "evening",
       price: "80₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Whether you're a fan of classic pepperoni, vegetarian, or looking for something a little more adventurous, our pizza has something for everyone. And with our generous portions and affordable prices, you can indulge in your favorite comfort food without breaking the bank.",
     },
     {
       id: 6,
@@ -52,7 +52,7 @@ const Menu = [
       category: "dinner",
       price: "180₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Our non-veg thaali is perfect for those looking to indulge in the rich and exotic flavors of Indian cuisine. Whether you're a fan of classic dishes or looking to try something new and exciting, our non-veg thaali has something for everyone.",
     },
     {
       id: 7,
@@ -61,7 +61,7 @@ const Menu = [
       category: "dinner",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "From our rich and creamy kulfi to our mouth-watering rasgulla, each of our sweets is a unique and flavorful experience that will transport you to a world of culinary delight. And with our generous portions and affordable prices, you can indulge in your favorite sweet treats without breaking the bank.",
     },
     {
       id: 8,
@@ -70,16 +70,16 @@ const Menu = [
       category: "lunch",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Experience the rich and hearty flavors of our Rajma Rice, the perfect comfort food for any occasion! Our Rajma Rice is made from tender kidney beans simmered to perfection in a savory blend of spices, served over a bed of fluffy, fragrant basmati rice.",
     },
     {
       id: 9,
       image: "../images/samosa.jpg",
-      name: "samaso",
+      name: "samosa",
       category: "evening",
       price: "10₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Our samosas are perfect for a quick snack or as an appetizer to share with friends and family. With its irresistible taste and aroma, it's no wonder why samosas are a beloved street food in many parts of the world. Come and savor the authentic taste of our samosas, freshly made and served hot for your enjoyment.",
     }
   ];
   
